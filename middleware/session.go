9e70f0e0b5dd9d57d9914e31c57f1f7655aa924c
wguan <wguan@mcirostrategy.com>
2025-03-10 23:12:19 +0800

    Use database as the session persist database

--- middleware/session.go
+++ middleware/session.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"workflow/configs"
+
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/postgres"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func SessionMiddleware() gin.HandlerFunc {
-	store := cookie.NewStore([]byte(viper.GetString("session_secret")))
+	// store:= cookie.NewStore([]byte(viper.GetString("session_secret")))
+	store, _ := postgres.NewStore(configs.DB)
 	return sessions.Sessions("session", store)
 }
